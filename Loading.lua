535a3f5bdc09b47cb0edbacceb10a57f4ba67914
João <jjaoppedro001@gmail.com>
2021-06-20 21:19:49 -0300

    New

--- Loading.lua
+++ Loading.lua
@@ -1,7 +1,10 @@
+--MM2
 if game.PlaceId == 6833753645 then
-
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Joaotipo/Ideia-unica/master/MM2.lua"))()
 end)
 
-if game.PlaceId == 264568898 then
+--Natural
 
+if game.PlaceId == 264568898 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Joaotipo/Ideia-unica/master/NaturalDisaster.lua"))()
 end)
