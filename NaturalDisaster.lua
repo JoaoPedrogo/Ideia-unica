ec5a3ea03c49015beacf580504f55fbbe802ca8e
João <jjaoppedro001@gmail.com>
2021-06-21 00:25:36 -0300

    New

--- NaturalDisaster.lua
+++ NaturalDisaster.lua
@@ -250,9 +250,6 @@ local function equip()
 	contextActionService:BindActionToInputTypes("Reload", function() 
 		game.ReplicatedStorage.ROBLOX_PistolReloadEvent:FireServer()		
 	end, true, "")
-	
-	-- If game uses filtering enabled then need to update server while tool is
-	-- held by character.
 	if workspace.FilteringEnabled then
 		while connection do
 			wait()
@@ -278,68 +275,17 @@ end))
 Script5.Name = "qPerfectionWeld"
 Script5.Parent = Tool0
 table.insert(cors,sandbox(Script5,function()
--- Created by Quenty (@Quenty, follow me on twitter).
--- Should work with only ONE copy, seamlessly with weapons, trains, et cetera.
--- Parts should be ANCHORED before use. It will, however, store relatives values and so when tools are reparented, it'll fix them.
-
---[[ INSTRUCTIONS
-- Place in the model
-- Make sure model is anchored
-- That's it. It will weld the model and all children. 
-
-THIS SCRIPT SHOULD BE USED ONLY BY ITSELF. THE MODEL SHOULD BE ANCHORED. 
-THIS SCRIPT SHOULD BE USED ONLY BY ITSELF. THE MODEL SHOULD BE ANCHORED. 
-THIS SCRIPT SHOULD BE USED ONLY BY ITSELF. THE MODEL SHOULD BE ANCHORED. 
-THIS SCRIPT SHOULD BE USED ONLY BY ITSELF. THE MODEL SHOULD BE ANCHORED. 
-THIS SCRIPT SHOULD BE USED ONLY BY ITSELF. THE MODEL SHOULD BE ANCHORED. 
-THIS SCRIPT SHOULD BE USED ONLY BY ITSELF. THE MODEL SHOULD BE ANCHORED. 
-THIS SCRIPT SHOULD BE USED ONLY BY ITSELF. THE MODEL SHOULD BE ANCHORED. 
-THIS SCRIPT SHOULD BE USED ONLY BY ITSELF. THE MODEL SHOULD BE ANCHORED. 
-
-This script is designed to be used is a regular script. In a local script it will weld, but it will not attempt to handle ancestory changes. 
-]]
-
---[[ DOCUMENTATION
-- Will work in tools. If ran more than once it will not create more than one weld.  This is especially useful for tools that are dropped and then picked up again.
-- Will work in PBS servers
-- Will work as long as it starts out with the part anchored
-- Stores the relative CFrame as a CFrame value
-- Takes careful measure to reduce lag by not having a joint set off or affected by the parts offset from origin
-- Utilizes a recursive algorith to find all parts in the model
-- Will reweld on script reparent if the script is initially parented to a tool.
-- Welds as fast as possible
-]]
-
--- qPerfectionWeld.lua
--- Created 10/6/2014
--- Author: Quenty
--- Version 1.0.3
-
--- Updated 10/14/2014 - Updated to 1.0.1
---- Bug fix with existing ROBLOX welds ? Repro by asimo3089
-
--- Updated 10/14/2014 - Updated to 1.0.2
---- Fixed bug fix. 
-
--- Updated 10/14/2014 - Updated to 1.0.3
---- Now handles joints semi-acceptably. May be rather hacky with some joints. :/
 
 local NEVER_BREAK_JOINTS = false -- If you set this to true it will never break joints (this can create some welding issues, but can save stuff like hinges).
 
 
 local function CallOnChildren(Instance, FunctionToCall)
-	-- Calls a function on each of the children of a certain object, using recursion.  
-
 	FunctionToCall(Instance)
-
 	for _, Child in next, Instance:GetChildren() do
 		CallOnChildren(Child, FunctionToCall)
 	end
 end
-
 local function GetNearestParent(Instance, ClassName)
-	-- Returns the nearest parent of a certain class, or returns nil
-
 	local Ancestor = Instance
 	repeat
 		Ancestor = Ancestor.Parent
@@ -347,31 +293,19 @@ local function GetNearestParent(Instance, ClassName)
 			return nil
 		end
 	until Ancestor:IsA(ClassName)
-
 	return Ancestor
 end
-
 local function GetBricks(StartInstance)
 	local List = {}
-
-	-- if StartInstance:IsA("BasePart") then
-	-- 	List[#List+1] = StartInstance
-	-- end
-
 	CallOnChildren(StartInstance, function(Item)
 		if Item:IsA("BasePart") then
 			List[#List+1] = Item;
 		end
 	end)
-
 	return List
 end
-
 local function Modify(Instance, Values)
-	-- Modifies an Instance by using a table.  
-
 	assert(type(Values) == "table", "Values is not a table");
-
 	for Index, Value in next, Values do
 		if type(Index) == "number" then
 			Value.Parent = Instance
@@ -381,16 +315,11 @@ local function Modify(Instance, Values)
 	end
 	return Instance
 end
-
 local function Make(ClassType, Properties)
-	-- Using a syntax hack to create a nice way to Make new items.  
-
 	return Modify(Instance.new(ClassType), Properties)
 end
-
 local Surfaces = {"TopSurface", "BottomSurface", "LeftSurface", "RightSurface", "FrontSurface", "BackSurface"}
 local HingSurfaces = {"Hinge", "Motor", "SteppingMotor"}
-
 local function HasWheelJoint(Part)
 	for _, SurfaceName in pairs(Surfaces) do
 		for _, HingSurfaceName in pairs(HingSurfaces) do
@@ -399,28 +328,21 @@ local function HasWheelJoint(Part)
 			end
 		end
 	end
-	
 	return false
 end
 
 local function ShouldBreakJoints(Part)
-	--- We do not want to break joints of wheels/hinges. This takes the utmost care to not do this. There are
-	--  definitely some edge cases. 
-
 	if NEVER_BREAK_JOINTS then
 		return false
 	end
-	
 	if HasWheelJoint(Part) then
 		return false
 	end
-	
 	local Connected = Part:GetConnectedParts()
 	
 	if #Connected == 1 then
 		return false
 	end
-	
 	for _, Item in pairs(Connected) do
 		if HasWheelJoint(Item) then
 			return false
@@ -428,21 +350,11 @@ local function ShouldBreakJoints(Part)
 			return false
 		end
 	end
-	
 	return true
 end
-
 local function WeldTogether(Part0, Part1, JointType, WeldParent)
-	--- Weld's 2 parts together
-	-- @param Part0 The first part
-	-- @param Part1 The second part (Dependent part most of the time).
-	-- @param [JointType] The type of joint. Defaults to weld.
-	-- @param [WeldParent] Parent of the weld, Defaults to Part0 (so GC is better).
-	-- @return The weld created.
-
 	JointType = JointType or "Weld"
 	local RelativeValue = Part1:FindFirstChild("qRelativeCFrameWeldValue")
-	
 	local NewWeld = Part1:FindFirstChild("qCFrameWeldThingy") or Instance.new(JointType)
 	Modify(NewWeld, {
 		Name = "qCFrameWeldThingy";
@@ -452,7 +364,6 @@ local function WeldTogether(Part0, Part1, JointType, WeldParent)
 		C1     = RelativeValue and RelativeValue.Value or Part1.CFrame:toObjectSpace(Part0.CFrame); --Part1.CFrame:inverse() * Part0.CFrame;-- Part1.CFrame:inverse();
 		Parent = Part1;
 	})
-
 	if not RelativeValue then
 		RelativeValue = Make("CFrameValue", {
 			Parent     = Part1;
@@ -461,28 +372,19 @@ local function WeldTogether(Part0, Part1, JointType, WeldParent)
 			Value      = NewWeld.C1;
 		})
 	end
-
 	return NewWeld
 end
-
 local function WeldParts(Parts, MainPart, JointType, DoNotUnanchor)
-	-- @param Parts The Parts to weld. Should be anchored to prevent really horrible results.
-	-- @param MainPart The part to weld the model to (can be in the model).
-	-- @param [JointType] The type of joint. Defaults to weld. 
-	-- @parm DoNotUnanchor Boolean, if true, will not unachor the model after cmopletion.
-	
 	for _, Part in pairs(Parts) do
 		if ShouldBreakJoints(Part) then
 			Part:BreakJoints()
 		end
 	end
-	
 	for _, Part in pairs(Parts) do
 		if Part ~= MainPart then
 			WeldTogether(MainPart, Part, JointType, MainPart)
 		end
 	end
-
 	if not DoNotUnanchor then
 		for _, Part in pairs(Parts) do
 			Part.Anchored = false
@@ -490,7 +392,6 @@ local function WeldParts(Parts, MainPart, JointType, DoNotUnanchor)
 		MainPart.Anchored = false
 	end
 end
-
 local function PerfectionWeld()	
 	local Tool = GetNearestParent(script, "Tool")
 
@@ -502,23 +403,14 @@ local function PerfectionWeld()
 	else
 		warn("qWeld - Unable to weld part")
 	end
-	
 	return Tool
 end
-
 local Tool = PerfectionWeld()
-
-
 if Tool and script.ClassName == "Script" then
-	--- Don't bother with local scripts
-
 	script.Parent.AncestryChanged:connect(function()
 		PerfectionWeld()
 	end)
 end
-
--- Created by Quenty (@Quenty, follow me on twitter).
-
 end))
 LocalScript6.Name = "Animate"
 LocalScript6.Parent = Tool0
@@ -529,7 +421,6 @@ local welds = {}
 local Tool = script.Parent
 local neck = nil
 local orginalC0 = CFrame.new(0, 1, 0, -1, -0, -0, 0, 0, 1, 0, 1, 0)
-
 function Equip(mouse)
 wait(0.01)
 arms = {Tool.Parent:FindFirstChild("Left Arm"), Tool.Parent:FindFirstChild("Right Arm")}
@@ -556,11 +447,9 @@ weld2.Part0 = head
 weld2.Parent = head
 weld2.Part1 = arms[2]
 welds[2] = weld2
--------------------------here
 weld1.C1 = CFrame.new(-0.5+1.5, 0.8, .9)* CFrame.fromEulerAnglesXYZ(math.rad(290), 0, math.rad(-90))
 weld2.C1 = CFrame.new(-1, 0.8, 0.5-1.5) * CFrame.fromEulerAnglesXYZ(math.rad(-90), math.rad(-15), 0)
 	mouse.Move:connect(function ()
-		--local Direction = Tool.Direction.Value 
 		local Direction = mouse.Hit.p
 		local b = head.Position.Y-Direction.Y
 		local dist = (head.Position-Direction).magnitude
@@ -575,7 +464,6 @@ else
 print("arms")
 end
 end
-
 function Unequip(mouse)
 if arms ~= nil and torso ~= nil then
 local sh = {torso:FindFirstChild("Left Shoulder"), torso:FindFirstChild("Right Shoulder")}
@@ -584,7 +472,6 @@ local yes = true
 if yes then
 yes = false
 	neck.C0 = orginalC0
-
 sh[1].Part1 = arms[1]
 sh[2].Part1 = arms[2]
 welds[1].Parent = nil
@@ -724,7 +611,6 @@ end))
 LocalScript8.Name = "MainScript"
 LocalScript8.Parent = Tool0
 table.insert(cors,sandbox(LocalScript8,function()
---Physics gun created by Killersoldier45
 wait() 
 tool = script.Parent
 lineconnect = tool.LineConnect
@@ -956,7 +842,6 @@ function onEquipped(mouse)
 	mouse.KeyDown:connect(function(key) onKeyDown(key,mouse) end)
 	mouse.Icon = "rbxassetid:// 509381906"
 end
-
 tool.Equipped:connect(onEquipped)
 end))
 Part9.Name = "GlowPart"
@@ -995,7 +880,6 @@ script.Parent.Transparency = .6
 wait(0.05)
 script.Parent.Transparency = .5
 end
-
 end))
 Part11.Name = "GlowPart"
 Part11.Parent = Tool0
@@ -1103,7 +987,7 @@ end
     local TeleSelec = tele:NewSection("Teleportes")
 
     TeleSelec:NewButton("Lobby", "Teleporte para o Lobby", function()
-    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-280, 170, 341, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-279.998871, 179.199951, 340.971771, 0.999999881, 3.22571259e-09, -0.000507893157, -3.18074211e-09, 1, 8.85443399e-08, 0.000507893157, -8.85427127e-08, 0.999999881)
 
 end)
 
